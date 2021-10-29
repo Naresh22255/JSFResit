8d59b37c96a786e31fad94cf443463e3f9165947
Naresh Kumar <naresh22255@gmail.com>
2021-10-29 23:58:32 +0200

    Project Outline

--- pokemon/src/App.js
+++ pokemon/src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Grass from "./components/Grass";
+import Header from "./components/Header";
+import Home from "./components/Home";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <h3>App</h3>
+      <Header />;
+      <Home />;
+      <Grass />;
+      <Contact />;
+      <Footer />;
+    </div>
+  );
 }
 
 export default App;
---
+++ pokemon/src/components/Grass.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Grass.css'
+
+function Grass() {
+  return (
+    <div className="grass">
+      <h2>Grass</h2>
+    </div>
+  );
+}
+
+export default Grass;
